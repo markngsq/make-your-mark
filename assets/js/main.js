e28d7a34d11c1057934b66575214721f44358d66
Calvin Ng <calvinnwq@gmail.com>
2015-02-19 01:55:31 +0800

    Commit the actual final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
 });
 
 function Terminal() {
+  this.hasEnded = false;
   this.currentLevel = "0";
-  this.currentScore = 5;
+  this.currentScore = 0;
   this.scoreUnitVal = 1;
   this.scoreThresholds = {
     "bad" : 3,
@@ -16,32 +17,32 @@ function Terminal() {
   };
   this.endingText = {
     "good" : {
-      image: "assets/img/top1.gif",
-      text: 'Your boss is amazed at the sheer ingenuity and innovation in the work! He says things like\n\n"seamless execution" and "this will change everything!".\n\nToo bad the client will never accept this.\n\nCONGRATULATIONS YOU HAVE GOT THE "GOOD" ENDING!'
+      image: "assets/img/goodend.gif",
+      text: 'Your boss is impressed by your work!\n As he applauds you, you feel a sense of achievement. \nFor some reason, this causes you to grow a beard.\nYOU HAVE GOT THE "GOOD" ENDING!'
     },
     "neutral" : {
-      image: "assets/img/top1.gif",
-      text: 'You did such an average job that your boss doesn\'t really know what to say about your\n\nwork. He begins nit picking on your choice of type and art direction...\n\nBasically he just wants you to start again and come up with a "better" idea.\n\nYOU GOT THE SO SO ENDING.'
+      image: "assets/img/neutralend.gif",
+      text: 'You did such an average job that your boss doesn\'t really know what to say. \nBasically he just wants you to start again and come up with a "better" idea.\nYOU GOT THE SO SO ENDING.'
     },
     "bad" : {
-      image: "assets/img/top1.gif",
-      text: 'Due to your negligence, your boss berates you on handing in something so shitty.\n\nAs he shreds your work to pieces in front of you, you think back to all the bad decisions\n\nthat you have made. TRY AGAIN (maybe you should make better decision)'
+      image: "assets/img/badend.gif",
+      text: 'Your boss is unimpressed by your work at all.\nAs he shreds your work to pieces in front of you, you feel your soul drifting away...\nTRY AGAIN (maybe you should make better decisions)'
     }
   };
   this.hints = {
-      level_0: "",
-      level_1: "Maybe you should try jaygo, computer, printer, nigel, procrastinate",
-      level_2: "chrome, internet explorer, photoshop",
-      level_3: "9gag, reddit, facebook, google",
-      level_4: "search inspiration, cat pictures",
-      level_5: "illustrator, indesign, photoshop",
-      level_5_1: "livetrace, vector, save",
-      level_5_2: "article, kern, package, save",
-      level_5_3: "user filters, touchup, brush tool, save",
-      level_6: "check settings, print",
-      level_6_1: "print",
-      level_6_2: "submit work, submit",
-    };
+    level_0: "",
+    level_1: "Don't be silly, try: jaygo, computer, printer, nigel, procrastinate",
+    level_2: "Really? try: chrome, internet explorer",
+    level_3: "Its on the screen and you can't get it? Try: 9gag, reddit, facebook, google",
+    level_4: "search inspiration, cat pictures",
+    level_5: "illustrator, indesign, photoshop",
+    level_5_1: "livetrace, vector, save",
+    level_5_2: "article, kern, package, save",
+    level_5_3: "use filters, touchup, brush tool, save",
+    level_6: "check settings, print",
+    level_6_1: "print",
+    level_6_2: "submit work, submit",
+  };
   this.commands = {
     level_0: [
       {
@@ -49,6 +50,7 @@ function Terminal() {
         outcome: "0-start"
       }
     ],
+    
     level_1: [
       {
         commands: ["jaygo"],
@@ -77,7 +79,7 @@ function Terminal() {
         outcome: "2-chrome"
       },
       {
-        commands: ["internet explorer", "open internet explorer"],
+        commands: ["internet explorer", "open internet explorer", "open explorer"],
         outcome: "2-explorer"
       },
       {
@@ -105,7 +107,7 @@ function Terminal() {
     ],
     level_4: [
       {
-        commands: ["search inspiration", "cat pictures"],
+        commands: ["inspiration", "search inspiration","cats", "cat pictures"],
         outcome: "4-search"
       }
     ],
@@ -157,7 +159,7 @@ function Terminal() {
     ],
     level_5_3: [
       {
-        commands: ["user filters"],
+        commands: ["use filters", "solarize", "filters"],
         outcome: "5-3-filters"
       },
       {
@@ -175,7 +177,7 @@ function Terminal() {
     ],
     level_6: [
       {
-        commands: ["check settings"],
+        commands: ["check settings", "settings"],
         outcome: "6-printsettings"
       },
       {
@@ -199,7 +201,7 @@ function Terminal() {
   this.outcomes = {
     // LEVEL 0 OUTCOMES
     "0-start": {
-      image: "assets/img/start.jpg",
+      image: "assets/img/top.gif",
       text: "",
       scoreType: "",
       nextLevel: "1"
@@ -207,32 +209,32 @@ function Terminal() {
     
     // LEVEL 1 OUTCOMES
     "1-jaygo": {
-      image: "assets/img/top3.png",
+      image: "assets/img/jaygo.gif",
       text: "(tutor) insert jaygo quote here",
       scoreType: "+",
       nextLevel: "1"
     },
     "1-computer": {
       image: "assets/img/on_computer.gif",
-      text: "It's alive!\n\nYou can see several applications on your screen.",
+      text: "It's alive!\nYou can see several applications on your screen.",
       scoreType: "+",
       nextLevel: "2"
     },
     "1-printer": {
-      image: "assets/img/top1.gif",
-      text: "Its a standard inkjet printer.\n\nNow's not the time to be using this yet...",
+      image: "assets/img/top.gif",
+      text: "Its a standard inkjet printer.\nNow's not the time to be using this yet...",
       scoreType: "",
       nextLevel: "1"
     },
     "1-nigel": {
-      image: "assets/img/top3.png",
+      image: "assets/img/nigel.gif",
       text: "(tutor) insert nigel quote",
       scoreType: "+",
       nextLevel: "1"
     },
     "1-procrastinate": {
-      image: "assets/img/top1.gif",
-      text: "You do nothing...",
+      image: "assets/img/top.gif",
+      text: "You do nothing... nothing at all.",
       scoreType: "-",
       nextLevel: "1"
     },
@@ -240,18 +242,18 @@ function Terminal() {
     // LEVEL 2 OUTCOMES
     "2-chrome": {
       image: "assets/img/chrome.gif",
-      text: "You look at your bookmarks tab.\n\nYou see 9gag, reddit, facebook, google\n\nWhat should I do...",
+      text: "You look at your bookmarks tab.\nYou see 9gag, reddit, facebook, google\nWhat should I do...",
       scoreType: "+",
       nextLevel: "3"
     },
     "2-explorer": {
       image: "assets/img/explorer.gif",
-      text: "Who the hell uses internet explorer?!\n\nIt 404's on you anyway.\n\nUse another browser you old man.",
+      text: "Who the hell uses internet explorer?!\nIt 404's on you anyway.\nUse another browser you old man.",
       scoreType: "-",
       nextLevel: "2"
     },
     "2-apps": {
-      image: "assets/img/on_computer.gif",
+      image: "assets/img/on_computer.png",
       text: "You can't do that yet.",
       scoreType: "",
       nextLevel: "2"
@@ -260,147 +262,147 @@ function Terminal() {
     // LEVEL 3 OUTCOMES
     "3-google": {
       image: "assets/img/google.png",
-      text: "Of course...\n\nWhat do I search for?",
+      text: "Of course...\nWhat do I search for?",
       scoreType: "+",
       nextLevel: "4"
     },
     "3-9gag": {
       image: "assets/img/9gag.png",
-      text: "You decide to go on 9gag.\n\nNow you are stuck forever scrolling..",
+      text: "You decide to go on 9gag.\nNow you are stuck forever scrolling..",
       scoreType: "-",
       nextLevel: "3"
     },
     "3-reddit": {
       image: "assets/img/reddit.png",
-      text: "You decide to go on reddit.\n\nYou got so distracted you decide to do something else instead.",
+      text: "You decide to go on reddit.\nYou got so distracted you decide to do something else instead.",
       scoreType: "-",
       nextLevel: "3"
     },
     "3-facebook": {
       image: "assets/img/facebook.png",
-      text: "You decide to go on Facebook.\n\nYou end up not doing any work.\n\nAt least you know about your friends' social lives now...",
+      text: "You decide to go on Facebook.\nYou end up not doing any work.\nAt least you know about your friends' social lives now...",
       scoreType: "-",
       nextLevel: "3"
     },
 
     // LEVEL 4 OUTCOMES
     "4-search": {
-      image: "assets/img/google.png",
-      text: "Somehow googling inspiration leads you to being inspired.\n\nWe all know you've just been searching for cat photos mostly...\n\nYou've obtained... A Picture of Cat\n\nMaybe I should draft a design?",
+      image: "assets/img/googleimage.gif",
+      text: "Somehow googling inspiration leads you to being inspired.\nWe all know you've just been searching for cat photos mostly...\nYou've obtained... A Picture of Cat\nMaybe I should draft a design?",
       scoreType: "",
       nextLevel: "5"
     },
 
     // LEVEL 5 OUTCOMES
     "5-illustrator": {
-      image: "assets/img/indesign.gif",
-      text: "You open adobe illustrator.\n\nWhat do i do in illustrator again?",
+      image: "assets/img/illustrator.gif",
+      text: "You open adobe illustrator.\nWhat do i do in illustrator again?",
       scoreType: "",
       nextLevel: "5_1"
     },
     "5-indesign": {
       image: "assets/img/indesign.gif",
-      text: "You open adobe indesign.\n\nYou need to write an article about cat.\n\nNow what?",
+      text: "You open adobe indesign.\nYou need to write an article about cat.\nNow what?",
       scoreType: "",
       nextLevel: "5_2"
     },
     "5-photoshop": {
       image: "assets/img/photoshop.gif",
-      text: "You open adobe photoshop.\n\nDon't go crazy on the filters...",
+      text: "You open adobe photoshop.\nDon't go crazy on the filters...",
       scoreType: "",
       nextLevel: "5_3"
     },
 
     // LEVEL 5-1 OUTCOMES
     "5-1-livetrace": {
-      image: "assets/img/indesign.gif",
+      image: "assets/img/livetrace.png",
       text: "Why would you livetrace anything?!",
       scoreType: "-",
       nextLevel: "5_1"
     },
     "5-1-vector": {
-      image: "assets/img/indesign.gif",
+      image: "assets/img/vector.png",
       text: "Nothings gonna stop you from blowing it up to A0 size now...",
       scoreType: "+",
       nextLevel: "5_1"
     },
     "5-1-save": {
-      image: "assets/img/indesign.gif",
-      text: "you save your file.\n\nI guess you gotta go send it to print now!",
+      image: "assets/img/illustrator.gif",
+      text: "you save your file.\nI guess you gotta go send it to print now!",
       scoreType: "",
-      nextLevel: "6"
+      nextLevel: "save_illustrator.gif"
     },
 
     // LEVEL 5-2 OUTCOMES
     "5-2-article": {
-      image: "assets/img/indesign.gif",
-      text: "You write an enthralling tale about cats.\n\nI mean if you didn't what's the alternative?\n\nUse Lorem Ipsum?",
+      image: "assets/img/article.png",
+      text: "You write an enthralling tale about cats.\nI mean if you didn't what's the alternative?\nUse Lorem Ipsum?",
       scoreType: "+",
       nextLevel: "5_2"
     },
     "5-2-kern": {
-      image: "assets/img/indesign.gif",
-      text: "You remembered to kern the article.\n\nIt looks so beautiful now.",
+      image: "assets/img/kern.png",
+      text: "You remembered to kern the article.\nIt looks so beautiful now.",
       scoreType: "+",
       nextLevel: "5_2"
     },
     "5-2-package": {
-      image: "assets/img/indesign.gif",
-      text: "You remembered to package the .idml file.\n\nThat was close! Imagine if you didn't do that!\n\nyour cats would have become placeholders",
+      image: "assets/img/package.png",
+      text: "You remembered to package the .idml file.\nThat was close! Imagine if you didn't do that!\nyour cats would have become placeholders",
       scoreType: "+",
       nextLevel: "5_2"
     },
     "5-2-save": {
-      image: "assets/img/indesign.gif",
-      text: "you save your file.\n\nI guess you gotta go send it to print now!",
+      image: "assets/img/save_indesign.gif",
+      text: "you save your file.\nI guess you gotta go send it to print now!",
       scoreType: "",
       nextLevel: "6"
     },
 
     // LEVEL 5-3 OUTCOMES
     "5-3-filters": {
-      image: "assets/img/photoshop.gif",
-      text: "You solarize the shit out of the photo.\n\nWOW.",
+      image: "assets/img/solarize.png",
+      text: "You solarize the shit out of the photo.\nWOW.",
       scoreType: "-",
       nextLevel: "5_3"
     },
     "5-3-touchup": {
-      image: "assets/img/photoshop.gif",
-      text: "You removed the acne of cat's face.\n\nSomehow you managed to turn cat into Ryan Gosling.",
+      image: "assets/img/rosling.png",
+      text: "You removed the acne of cat's face.\nSomehow you managed to turn cat into Ryan Gosling.",
       scoreType: "+",
       nextLevel: "5_3"
     },
     "5-3-paint": {
-      image: "assets/img/photoshop.gif",
-      text: "You decide to digitally repaint the picture of cat.\n\nSeriously, are you an illustrator or a graphic designer?! (or are you both?)",
+      image: "assets/img/paint.png",
+      text: "You decide to digitally repaint the picture of cat.\nSeriously, are you an illustrator or a graphic designer?! (or are you both?)",
       scoreType: "+",
       nextLevel: "5_3"
     },
     "5-3-save": {
-      image: "assets/img/photoshop.gif",
-      text: "you save your file.\n\nI guess you gotta go send it to print now!",
+      image: "assets/img/save_photoshop.gif",
+      text: "you save your file.\nI guess you gotta go send it to print now!",
       scoreType: "",
       nextLevel: "6"
     },
 
     // LEVEL 6 OUTCOMES
     "6-printsettings": {
-      image: "assets/img/top1.gif",
-      text: "You check your print settings.\n\nLucky you! The settings were all horribly wrong.\n\nYou set it to the correct print settings",
+      image: "assets/img/printsettings.gif",
+      text: "You check your print settings.\nLucky you! The settings were all horribly wrong.\nYou set it to the correct print settings",
       scoreType: "+",
       nextLevel: "6_1"
     },
     "6-print": {
-      image: "assets/img/top1.gif",
-      text: "In your lapse of judgement you forgot to check your print settings.\n\nYour printer starts spraying ink all over!\n\nFor some reason it still prints out but it just looks fugly.",
+      image: "assets/img/no_printsettings.gif",
+      text: "In your lapse of judgement you forgot to check your print settings.\nYour printer starts spraying ink all over!\nFor some reason it still prints out but it just looks fugly.",
       scoreType: "-",
       nextLevel: "6_2"
     },
 
     // LEVEL 6-1 OUTCOMES
     "6-1-print": {
-      image: "assets/img/top1.gif",
-      text: "Lucky for you, you checked your print settings.\n\nThe printer prints out the image beautifully.\n\nYou marvel at its beauty.",
+      image: "assets/img/printsettings.gif",
+      text: "Lucky for you, you checked your print settings.\nThe printer prints out the image beautifully.\nYou marvel at its beauty.",
       scoreType: "",
       nextLevel: "6_2"
     },
@@ -410,7 +412,7 @@ function Terminal() {
       image: "assets/img/top1.gif",
       text: "Your boss appears out of nowhere!",
       scoreType: "",
-      nextLevel: "0"
+      nextLevel: "end"
     }
   };
 }
@@ -426,10 +428,16 @@ Terminal.prototype.init = function() {
     // Get all the commands for the level.
     var levelCommands = that.commands["level_" + that.currentLevel];
     
-    console.log(that.currentLevel);
-    console.log(levelCommands);
+    // If ended and key pressed go back to start
+    if (that.currentLevel === "end" && that.hasEnded) {
+      that.hasEnded = false;
+      that.currentLevel = "0";
+      that.currentScore = 0;
+      $('#visual img').attr('src', "assets/img/startpage.png");
+      return;
+    }
 
-    if (command === "/hints") {
+    if (command === "hints") {
       term.echo(that.hints["level_" + that.currentLevel]);
     } else {
       tries++;
@@ -440,7 +448,6 @@ Terminal.prototype.init = function() {
           // Found
           outcome = that.outcomes[commandItem.outcome];
           hasCommand = true;
-          return true;
         }
       });
 
@@ -466,6 +473,7 @@ Terminal.prototype.init = function() {
 
         // Check if ended
         if (that.currentLevel === "end") {
+          that.hasEnded = true;
           if (that.currentScore <= that.scoreThresholds["bad"]) {
             $('#visual img').attr('src', that.endingText["bad"].image);
             term.echo(that.endingText["bad"].text);
@@ -478,11 +486,12 @@ Terminal.prototype.init = function() {
           }
         }
       } else {
+        console.log(tries);
         if (tries === 3) {
           term.echo(that.hints["level_" + that.currentLevel]);
           tries = 0;
         } else {
-          term.echo("I don't know what " + command + " means.");
+          term.echo("What does " + command + " mean?");
         }
       }
     }
